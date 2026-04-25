7b22953c5354547a4d2bcb70bd5cd8e2b4293d91
Kaito420 <kaito420.lkr@gmail.com>
2026-04-25 11:18:24 +0900

    マップの自動生成、自動移動機能追加

---
+++ EL4S_01_Team07/Assets/Scripts/MapManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject[] _maps;
+    private GameObject _currentField;
+    private GameObject _nextField;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _currentField = Instantiate(_maps[0], Vector3.zero, Quaternion.identity);
+        _nextField = Instantiate(_maps[0], new Vector3(18, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentField.transform.Translate(Vector3.left * Time.deltaTime * 5);
+        _nextField.transform.Translate(Vector3.left * Time.deltaTime * 5);
+
+        if (_currentField.transform.position.x < -18.0f)
+        {
+            Destroy(_currentField);
+            _currentField = _nextField;
+            _nextField = Instantiate(_maps[0], new Vector3(18, 0, 0), Quaternion.identity);
+        }
+    }
+}
