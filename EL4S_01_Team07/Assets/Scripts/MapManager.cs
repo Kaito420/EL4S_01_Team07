c35dbc178cef6e1e0805ed7fcb0709fe5e6aa46a
tetoyama <164453224+tetoyama@users.noreply.github.com>
2026-04-25 17:00:20 +0900

    Update MapManager.cs

--- EL4S_01_Team07/Assets/Scripts/MapManager.cs
+++ EL4S_01_Team07/Assets/Scripts/MapManager.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class MapManager : MonoBehaviour
 {
     public float scrollSpeed = 5.0f;
     public float moveDistance = 0.0f;
 
-
+    public List<GameObject> old;
     public GameObject[] _maps;
     private GameObject _currentField;
     public GameObject CurrentField => _currentField;
@@ -25,12 +26,22 @@ public class MapManager : MonoBehaviour
 
         _currentField.transform.Translate(Vector3.left * Time.deltaTime * scrollSpeed);
         _nextField.transform.Translate(Vector3.left * Time.deltaTime * scrollSpeed);
+
+        foreach (GameObject field in old)
+        {
+            if (field != null)
+            {
+                field.transform.Translate(Vector3.left * Time.deltaTime * scrollSpeed);
+            }
+        }
+
         moveDistance += Time.deltaTime * scrollSpeed;
 
         if (_currentField.transform.position.x <= -18.0f)
         {
             float offset = _currentField.transform.position.x + 18.0f * 2.0f; // Calculate the offset to ensure smooth transition
-            Destroy(_currentField);
+            Destroy(_currentField,10.0f);
+            old.Add(_currentField);
             _currentField = _nextField;
             _nextField = Instantiate(_maps[Random.Range(0, _maps.Length)], new Vector3(offset, 0, 0), Quaternion.identity);
         }
